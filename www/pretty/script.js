ff2ffec7b3df49c0484a847a7811183c48906ed9
tomt <65259421+imtomt@users.noreply.github.com>
2026-04-14 17:04:51 -0700

    removed www/ folder because its just for local testing

--- www/pretty/script.js
+++
@@ -1,6 +0,0 @@
-// script.js
-let count = 0;
-document.getElementById('btn').addEventListener('click', () => {
-    count++;
-    document.getElementById('output').textContent = 'clicked ' + count + ' times';
-});
